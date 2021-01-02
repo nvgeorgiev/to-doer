1d2366dc43d5e387a38e5d9049b3c5e194082ed0
Nikolay Georgiev <nikolaay.georgiev@gmail.com>
2021-01-02 17:02:28 +0200

    Connected the app to the firebase database

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, FormControl, Input, InputLabel } from '@material-ui/core';
-import './App.css';
+import firebase from 'firebase';
 
+import './App.css';
+import db from './firebase';
 import Task from './components/Task/Task';
 
 function App() {
-  const [tasks, setTasks] = useState([
-    'Take dogs for a walk',
-    'Take the rubbish out',
-    'Grocery shopping',
-  ]);
-
+  const [tasks, setTasks] = useState([]);
   const [input, setInput] = useState('');
+
+  // when the app loads, we need to listen to the database and fetch new tasks as they get added/removed
+  useEffect(() => {
+    //this code here...fires when the app loads
+    db.collection('tasks')
+      .orderBy('timestamp', 'desc')
+      .onSnapshot((snapshot) => {
+        setTasks(
+          snapshot.docs.map((doc) => ({ id: doc.id, task: doc.data().task }))
+        );
+      });
+  }, []);
 
   const addTask = (event) => {
     // this will fire off when we click the button "Add Task"
     event.preventDefault(); // stop the REFRESH of the page and keep the short term memory
-    setTasks([...tasks, input]);
+
+    db.collection('tasks').add({
+      task: input,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+
     setInput(''); // clear up the input field after pressing the "Add Task" button
   };
 
@@ -46,7 +60,9 @@ function App() {
 
       <ul>
         {tasks.map((task) => (
-          <Task text={task} />
+          <div>
+            <Task task={task} />
+          </div>
         ))}
       </ul>
     </div>
--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,20 +1,54 @@
-import React from 'react';
-import {
-  List,
-  ListItem,
-  ListItemText,
-  ListItemAvatar,
-} from '@material-ui/core';
+import React, { useState } from 'react';
+import { List, ListItem, ListItemText, Modal } from '@material-ui/core';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+import { makeStyles } from '@material-ui/core/styles';
+
 import './Task.css';
+import db from '../../firebase';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'ablosute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function Task(props) {
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
   return (
-    <List className="Task">
-      <ListItem>
-        <ListItemAvatar></ListItemAvatar>
-        <ListItemText primary={props.text} secondary="Dummy deadline ⏰" />
-      </ListItem>
-    </List>
+    <div>
+      <Modal open={open} onClose={(event) => setOpen(false)}>
+        <div>
+          <h1>I am a modal</h1>
+          <button>sss</button>
+        </div>
+      </Modal>
+      <List className="Task">
+        <ListItem>
+          <ListItemText
+            primary={props.task.task}
+            secondary="Dummy deadline ⏰"
+          />
+          <button onClick={handleOpen}>Edit</button>
+          <DeleteForeverIcon
+            className="DeleteForeverIcon"
+            onClick={(event) =>
+              db.collection('tasks').doc(props.task.id).delete()
+            }
+          />
+        </ListItem>
+      </List>
+    </div>
   );
 }
 
